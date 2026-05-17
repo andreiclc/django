3c769727ce798415b36bc8cb5be35173616f3c89
Andrei <andrei.cosa93@gmail.com>
2026-05-17 12:09:25 +0300

    tema_30 - book model, add and display books

--- arhiva_teme/urls.py
+++ arhiva_teme/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from tema_28 import views as views_28
 from tema_29 import views as views_29
+from tema_30 import views as views_30
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tema-28/', views_28.exercitiu),
     path('tema-29/', views_29.exercitiu),
+    path('tema-30/', views_30.books, name='books'),
+    path('tema-30/add/', views_30.add_books, name='add_books'),
 ]
--- tema_30/views.py
+++ tema_30/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime, timezone
+from tema_30.models import Book
+from root import CONST
 
-# Create your views here.
+
+def add_books(request):
+    books = [
+        {
+            'name': 'Dune',
+            'author': 'Frank Herbert',
+            'release_date': datetime(1965, 8, 1, tzinfo=timezone.utc)
+        },
+        {
+            'name': 'Harry Potter',
+            'author': 'J.K. Rowling',
+            'release_date': datetime(1997, 6, 26, tzinfo=timezone.utc)
+        },
+        {
+            'name': 'Lord of the Rings',
+            'author': 'J.R.R. Tolkien',
+            'release_date': datetime(1954, 7, 29, tzinfo=timezone.utc)
+        },
+        {
+            'name': 'Mistborn: The Final Empire',
+            'author': 'Brandon Sanderson',
+            'release_date': datetime(2006, 7, 17, tzinfo=timezone.utc)
+        },
+        {
+            'name': 'The Witcher: The Last Wish',
+            'author': 'Andrzej Sapkowski',
+            'release_date': datetime(1993, 1, 1, tzinfo=timezone.utc)
+        },
+    ]
+
+    for book in books:
+        Book.objects.create(**book)
+
+    return redirect('books')
+
+
+def books(request):
+    """randare template exercitiu.html."""
+    all_books = Book.objects.all()
+    context = {
+        'titlu': CONST['nr_tema_30'],
+        'version': CONST['version'],
+        'books': all_books,
+    }
+    return render(request, 'tema_30/exercitiu.html', context)
+
+
+
+
+
+
+
